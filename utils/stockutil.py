2cfffcf9c79d538f2475bcbe57120eb7314cabc4
kongdw <kong.dw@hotmail.com>
2016-11-28 17:43:41 +0800

    添加交易账号代码

--- utils/stockutil.py
+++ utils/stockutil.py
@@ -10,8 +10,8 @@ def get_stock_type(stock_code):
     :param stock_code:股票ID, 若以 'sz', 'sh' 开头直接返回对应类型，否则使用内置规则判断
     :return 'sh' or 'sz'"""
     assert type(stock_code) is str, 'stock code need str type'
-    if stock_code.startswith(('sh', 'sz')):
-        return stock_code[:2]
+    if stock_code.startswith(('sh', 'sz','SH','SZ')):
+        return stock_code[:2].lower()
     if stock_code.startswith(('50', '51', '60', '90', '110', '113', '132', '204')):
         return 'sh'
     if stock_code.startswith(('00', '13', '18', '15', '16', '18', '20', '30', '39', '115', '1318')):
