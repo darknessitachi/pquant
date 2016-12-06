e2fd05ca73a8107e71635ecf23ed5561809c3a80
kongdw <kong.dw@hotmail.com>
2016-12-06 18:57:59 +0800

    增加事件引擎

--- strategies/网格交易策略.py
+++ strategies/网格交易策略.py
@@ -1,40 +1,76 @@
-import datetime as dt
-from dateutil import tz
-from utils.strategyTemplate import StrategyTemplate
+from utils.strategyTemplate import StrategyTemplate, StrategyState
+from collections import defaultdict
+import math
 import os
+import logging
 import utils.commutil as cutils
+import utils.stockutil as sutils
 
 
 class Strategy(StrategyTemplate):
     name = '网格交易策略'
+    current_path = os.path.dirname(__file__)
+    tmp_file_path = current_path + '/' + name + '.tmp'
+    config_file_path = current_path + '/' + name + '.json'
+    config = cutils.file2dict(config_file_path)
+    g = StrategyState()
 
-    def init(self):
-        self.strategy_config_path = os.path.dirname(__file__) + '/{}.json'.format(self.name)
-        self.strategy_config = cutils.file2dict(self.strategy_config_path)
-        # 通过下面的方式来获取时间戳
-        now_dt = self.clock_engine.now_dt
-        now = self.clock_engine.now
-        # 注册时钟事件
-        clock_type = "盘尾"
-        moment = dt.time(14, 56, 30, tzinfo=tz.tzlocal())
-        self.clock_engine.register_moment(clock_type, moment)
-        # 注册时钟间隔事件, 不在交易阶段也会触发, clock_type == minute_interval
-        minute_interval = 1.5
-        self.clock_engine.register_interval(minute_interval, trading=False)
+    def initialize(self):
+        self.log = logging.getLogger(self.name)
         self.subscribe()
+        if os.path.exists(self.tmp_file_path):
+            self.g = self.unserialize(self.tmp_file_path)
+        else:
+            self.g.stocks = defaultdict(dict)
+            for key in self.config.keys():
+                self.g.stocks.setdefault(key, {'lastNet': 0, 'upPrice': self._calcPrice(key, 1),
+                                               'downPrice': self._calcPrice(key, -1)})
 
     def subscribe(self):
         for quotation in self.quotation_engines:
-            quotation.subscribe(['150176', '600887', '600315'])
-        pass
+            quotation.subscribe(['150176'])
+
+    def _calcPrice(self, stock, net):
+        rate = math.pow(1 + self.config[stock]['upSize'] / 100, net) if net > 0 else 1 / math.pow(
+            1 + self.config[stock]['downSize'] / 100, -net)
+        return sutils.ensure_price(stock, self.config[stock]['initPrice'] * rate)
+
+    def checkBuyOrSell(self, stock, price):
+        upPrice = self.g.stocks[stock]['upPrice']
+        downPrice = self.g.stocks[stock]['downPrice']
+        lastNet = self.g.stocks[stock]['lastNet']
+        print(upPrice, downPrice)
+        if price >= self.g.stocks[stock]['upPrice']:
+            # 卖
+            self.log.info("网格{},卖出{},单价{},金额{}".format(lastNet,stock,price,self._calcNumber(stock,lastNet,'buy')))
+            self.setLastNet(stock, lastNet + 1)
+        elif price <= self.g.stocks[stock]['downPrice']:
+            # 买
+            self.log.info("网格{},买入{},单价{},金额:{}".format(lastNet,stock, price, self._calcNumber(stock, lastNet, 'sell')))
+            self.setLastNet(stock, self.g.stocks[stock]['lastNet'] - 1)
+
+    def setLastNet(self, stock, net):
+        self.g.stocks[stock]['lastNet'] = net
+        self.g.stocks[stock]['upPrice'] = self._calcPrice(stock, net + 1)
+        self.g.stocks[stock]['downPrice'] = self._calcPrice(stock, net - 1)
+
+    def _calcNumber(self, stock, net, type):
+        downVal = self.config[stock]['downVal']
+        upVal = self.config[stock]['upVal']
+        rate = math.pow(1 + self.config[stock]['valCoefficient'] / 100, abs(net))
+        if type == 'buy':
+            return rate * downVal
+        else:
+            return rate * upVal
 
     def strategy(self, event):
-
-        self.log.info('{}'.format(self.name))
-        # self.log.info('行情数据: H股B: %s' % event.data['150176'])
-        self.log.info('检查持仓')
-        # self.log.info(self.user.balance)
-        self.log.info('\n')
+        for stock in event.data.keys():
+            self.checkBuyOrSell(stock, event.data[stock]['now'])
+            self.log.info('{}'.format(event.data))
+            # self.log.info('行情数据: H股B: %s' % event.data['150176'])
+            self.log.info('检查持仓')
+            # self.log.info(self.user.balance)
+            self.log.info('\n')
 
     def clock(self, event):
         """在交易时间会定时推送 clock 事件
@@ -61,4 +97,18 @@ class Strategy(StrategyTemplate):
         关闭进程前的调用
         :return:
         """
-        self.log.info("假装在关闭前保存了策略数据")
+        self.serialize(self.tmp_file_path, self.g)
+        self.log.info("策略已经序列化到临时文件:{}".format(self.tmp_file_path))
+
+    @staticmethod
+    def serialize(file, obj):
+        import pickle
+        f = open(file, 'wb')
+        pickle.dump(obj, f)
+
+    @staticmethod
+    def unserialize(file):
+        import pickle
+        f = open(file, 'rb')
+        data = pickle.load(f)
+        return data
